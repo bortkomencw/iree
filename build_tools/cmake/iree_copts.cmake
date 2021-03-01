bortkomencw/iree

commit f03c3c12c51e5013c12275694e26e7cac195da53

Use separate Bazel invocation for TF integration binaries (#4836)
Rather than having CMake invoke Bazel itself, require that the
necessary binaries be built prior to running CMake and then import
them.

This is similar to what https://github.com/google/iree/pull/4584 did
in the Android build and has similar advantages: the user can get these
binaries in a variety of ways: building with their chosen config (e.g.
with local or remote caching), installing prebuilt binaries,  etc.

It also is significantly more clear what's going on IMO. Having CMake
invoke Bazel has caused a fair amount of confusion.

Incidentally, I'm pretty sure this increases test coverage by enabling
TF tests that were apparently not getting run.

Tested:
In addition to the CI, ran all the release distribution commands
locally and confirmed they still exited successfully.

build_tools/bazel/iree.bazelrc
@@ -155,6 +155,20 @@ build:macos_clang --per_file_copt=tensorflow,iree_tf_compiler@-Wno-unused-variab
 build:macos_clang --per_file_copt=tensorflow,iree_tf_compiler@-Wno-range-loop-analysis
 
 ###############################################################################
+# Additional options for release builds. These do *not* automatically pull in
+# the corresponding compiler config (e.g. generic_clang) because this is likely
+# to already be set in the user's configured.bazelrc and setting a config twice
+# causes flags to be passed twice.
+###############################################################################
+build:generic_clang_release --compilation_mode=opt
+build:generic_clang_release --linkopt=-Wl,--strip-all
+build:generic_gcc_release --compilation_mode=opt
+build:generic_gcc_release --linkopt=-Wl,--strip-all
+# MSVC doesn't store symbols in the binary (they're in separate .pdb files), so
+# nothing needs to be stripped.
+build:msvc_release --compilation_mode=opt
+
+###############################################################################
 # Options for building with address sanitizer.
 # https://github.com/google/sanitizers/wiki/AddressSanitizer
 ###############################################################################
@@ -302,6 +316,9 @@ build:rs --project_id=iree-oss
 ###############################################################################
 
 build:_msvc_base --define=iree_is_msvc=true
+
+# Bazel gets confused and sometimes loses track of Bash.
+build:_msvc_base --action_env BAZEL_SH='bash'
 
 # Disable warnings for dependencies. We don't control these.
 # absl forces /W3 in their copts, so we exclude them to avoid D9025

.bazelrc
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Import the main bazelrc config. This is in a separate file so that it's
-# possible to turn off all user bazelrc options by specifying
+# possible to turn off some or all user and system bazelrc options by specifying
 # `--nosystem_rc --nohome_rc --noworkspace_rc --bazelrc=build_tools/bazel/iree.bazelrc`
 try-import %workspace%/build_tools/bazel/iree.bazelrc
 

.github/workflows/build_package.yml
@@ -142,6 +142,13 @@ jobs:
           EOF
           cat ./main_checkout/version_info.json
 
+      - name: Configure Bazel
+        if: matrix.needs_bazel
+        shell: bash
+        run: |
+          cd main_checkout
+          python ./configure_bazel.py
+
       # The main distribution builds the main binary package, tests and compiler
       # wheels (which are OS but not python version specific). The latter is
       # purely opportunistic: it adds incrementally to the former and would

CMakeLists.txt
@@ -371,16 +371,6 @@ if(${IREE_BUILD_PYTHON_BINDINGS})
 endif()
 
 #-------------------------------------------------------------------------------
-# Bazel setup (conditional on whether features need it)
-# Depends on python configuration.
-#-------------------------------------------------------------------------------
-
-if(${IREE_ENABLE_TENSORFLOW})
-  include(configure_bazel)
-  iree_configure_bazel()
-endif()
-
-#-------------------------------------------------------------------------------
 # Other dependencies.
 #-------------------------------------------------------------------------------
 

build_tools/cmake/configure_bazel.cmake
@@ -1,204 +1,0 @@
-# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set(IREE_BAZEL_EXECUTABLE "bazel"
-    CACHE STRING "Bazel executable to use for bazel builds")
-set(IREE_BAZEL_SH "bash"
-    CACHE STRING "Bash command for bazel (on Windows cannot be system32 bash)")
-
-# iree_configure_bazel
-#
-# Configures the CMake binary directory to also contain a bazel build root.
-# The following files will be created:
-#   bazel (shell script): Shell wrapper to invoke bazel
-#   bazel.bat: Windows batch file to invoke bazel
-#   bazelrc: The bazelrc to use for the build
-#   bazel-out/: Bazel output directory
-#   bazel-bin/: Symlink to the bin directory appropriate for the build mode
-#
-# Variables will be set in the parent scope:
-#   IREE_BAZEL_WRAPPER: Executable wrapper to invoke to run bazel
-#   IREE_BAZEL_BIN: Path to the bazel-bin directory
-function(iree_configure_bazel)
-  message(STATUS "Using bazel executable: ${IREE_BAZEL_EXECUTABLE}")
-  set(_bazel_output_base "${CMAKE_BINARY_DIR}/bazel_output_base")
-  set(IREE_BAZEL_SRC_ROOT "${CMAKE_SOURCE_DIR}/integrations/tensorflow")
-
-  # Configure comilation mode.
-  set(_bazel_compilation_mode_opt "")
-  set(_bazel_strip_opt "")
-  if("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
-    message(STATUS "Enabling bazel release mode")
-    set(_bazel_compilation_mode_opt "build --compilation_mode=opt")
-    # Note: Bazel --strip is not strip.
-    # https://docs.bazel.build/versions/master/user-manual.html#flag--strip
-    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-      set(_bazel_strip_opt "build --linkopt=-Wl,--strip-all")
-    endif()
-  endif()
-
-  # Use the utility to emit _bazelrc_file configuration options.
-  set(_bazelrc_file "${CMAKE_BINARY_DIR}/bazelrc")
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    set(_bazel_platform_config generic_clang)
-  elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
-    set(_bazel_platform_config macos_clang)
-  elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    message(WARNING "Configuring bazel build for GCC: This receives minimal testing (recommend clang)")
-    set(_bazel_platform_config generic_gcc)
-  elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    set(_bazel_platform_config windows)
-  else()
-    message(WARNING "Did not recognize C++ compiler ${CMAKE_CXX_COMPILER_ID}. Configuring bazel for gcc mode and hoping for the best.")
-    set(_bazel_platform_config generic_gcc)
-  endif()
-  # Now add an import to the configured.bazelrc to load the project-wide
-  # bazelrc file.
-  # Note that the PYTHON_BIN_PATH is for TensorFlow's benefit on
-  # Windows. Just because. Everything has to be special. And the BAZEL_SH
-  # *HAS* to be non system32 bash (WSL). Because, why? Just because.
-  file(WRITE "${_bazelrc_file}" "
-build --config ${_bazel_platform_config}
-build --progress_report_interval=30
-build --python_path='${Python3_EXECUTABLE}'
-build --action_env BAZEL_SH='${IREE_BAZEL_SH}'
-build --action_env PYTHON_BIN_PATH='${Python3_EXECUTABLE}'
-build --action_env CC='${CMAKE_C_COMPILER}'
-build --action_env CXX='${CMAKE_CXX_COMPILER}'
-build --experimental_convenience_symlinks=ignore
-${_bazel_compilation_mode_opt}
-${_bazel_strip_opt}
-import ${CMAKE_SOURCE_DIR}/build_tools/bazel/iree.bazelrc
-")
-
-  # Note that we do allow a .bazelrc in the home directory (otherwise we
-  # would have --nohome_rc). This is mainly about disabling interference from
-  # interactive builds in the workspace.
-  set(_bazel_startup_options --nosystem_rc --noworkspace_rc "--bazelrc=${_bazelrc_file}" "--output_base=${_bazel_output_base}")
-
-  # And emit scripts to delegate to bazel.
-  set(IREE_BAZEL_WRAPPER "${CMAKE_BINARY_DIR}/bazel")
-  string(REPLACE ";" " " _bazel_startup_options_joined "${_bazel_startup_options}")
-  configure_file(
-    "${CMAKE_CURRENT_SOURCE_DIR}/build_tools/cmake/bazel.sh.in"
-    "${IREE_BAZEL_WRAPPER}"
-  )
-  configure_file(
-    "${CMAKE_CURRENT_SOURCE_DIR}/build_tools/cmake/bazel.bat.in"
-    "${IREE_BAZEL_WRAPPER}.bat"
-  )
-  if(NOT WIN32)
-    execute_process(
-      COMMAND chmod a+x "${IREE_BAZEL_WRAPPER}"
-    )
-  endif()
-
-  # On windows, the vagaries of tunneling through cmd.exe are not worth it
-  # (nested quoting...), so we just invoke bazel directly and accept that
-  # there can be a variance with an interactive launch.
-  if(WIN32)
-    set(IREE_BAZEL_COMMAND ${IREE_BAZEL_EXECUTABLE} ${_bazel_startup_options})
-  else()
-    set(IREE_BAZEL_COMMAND ${IREE_BAZEL_WRAPPER})
-  endif()
-
-  message(STATUS "Full bazel command: ${IREE_BAZEL_COMMAND}")
-  # Now ready to start bazel and ask it things.
-  message(STATUS "Detecting bazel version...")
-  execute_process(
-    RESULT_VARIABLE RC
-    OUTPUT_VARIABLE BAZEL_RELEASE
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    WORKING_DIRECTORY "${IREE_BAZEL_SRC_ROOT}"
-    COMMAND ${IREE_BAZEL_COMMAND} info release)
-  if(NOT RC EQUAL 0)
-    message(FATAL_ERROR "Failed to launch bazel: ${IREE_BAZEL_COMMAND}")
-  endif()
-  execute_process(
-    RESULT_VARIABLE RC
-    OUTPUT_VARIABLE IREE_BAZEL_BIN
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    WORKING_DIRECTORY "${IREE_BAZEL_SRC_ROOT}"
-    COMMAND ${IREE_BAZEL_COMMAND} info bazel-bin)
-  if(NOT RC EQUAL 0)
-    message(FATAL_ERROR "Failed to run 'info bazel-bin' via ${IREE_BAZEL_COMMAND}")
-  endif()
-  message(STATUS "Found bazel ${BAZEL_RELEASE}, bin directory: ${IREE_BAZEL_BIN}")
-  message(STATUS "Bazel wrapper script generated at: ${IREE_BAZEL_WRAPPER}")
-
-  # Build automation will use the IREE_BAZEL_BIN variable, but also drop a
-  # convenience symlink, since that is what people expect.
-  # And since bazel isn't nice enough to create it...
-  if(NOT WIN32)
-    execute_process(
-      RESULT_VARIABLE RC
-      COMMAND
-        ln -sf "${IREE_BAZEL_BIN}" "${CMAKE_CURRENT_BINARY_DIR}/bazel-bin"
-    )
-    if(NOT RC EQUAL 0)
-      message(WARNING "Failed to create convenience bazel-bin symlink")
-    endif()
-  endif()
-
-  set(IREE_BAZEL_SRC_ROOT "${IREE_BAZEL_SRC_ROOT}" PARENT_SCOPE)
-  set(IREE_BAZEL_WRAPPER "${IREE_BAZEL_WRAPPER}" PARENT_SCOPE)
-  set(IREE_BAZEL_COMMAND "${IREE_BAZEL_COMMAND}" PARENT_SCOPE)
-  set(IREE_BAZEL_BIN "${IREE_BAZEL_BIN}" PARENT_SCOPE)
-endfunction()
-
-# iree_add_bazel_invocation
-#
-# Adds a target to perform a bazel invocation, building a list of targets
-# and exporting pseudo targets for some results of the build.
-#
-# Parameters:
-#   INVOCATION_TARGET: The target name for the custom invocation target.
-#   BAZEL_TARGETS: List of bazel targets to build.
-#   EXECUTABLE_PATHS: Paths under bazel-bin for executables. An equivalent
-#     CMake imported executable target will be created for each by replacing
-#     the "/" with "_".
-function(iree_add_bazel_invocation)
-  cmake_parse_arguments(ARG
-    "ALL"
-    "INVOCATION_TARGET"
-    "BAZEL_TARGETS;EXECUTABLE_PATHS"
-    ${ARGN}
-  )
-  set(_all_option)
-  if(${ARG_ALL})
-    set(_all_option "ALL")
-  endif()
-  add_custom_target(${ARG_INVOCATION_TARGET} ${_all_option}
-    USES_TERMINAL
-    WORKING_DIRECTORY "${IREE_BAZEL_SRC_ROOT}"
-    COMMAND ${CMAKE_COMMAND} -E echo
-        "Starting bazel build of targets '${ARG_BAZEL_TARGETS}'"
-    COMMAND ${IREE_BAZEL_COMMAND} build -k ${ARG_BAZEL_TARGETS}
-    COMMAND ${CMAKE_COMMAND} -E echo "Bazel build complete."
-  )
-
-  # Create an imported executable target for each binary path.
-  # Since the bazel directory namespace lines up with the cmake namespace,
-  # generate a cmake target name for each.
-  foreach(_executable_path ${ARG_EXECUTABLE_PATHS})
-    string(REPLACE "/" "_" _executable_target "${_executable_path}")
-    message(STATUS "Add bazel executable target ${_executable_target}")
-    add_executable(${_executable_target} IMPORTED GLOBAL)
-    set_target_properties(${_executable_target}
-        PROPERTIES IMPORTED_LOCATION
-            "${IREE_BAZEL_BIN}/${_executable_path}${CMAKE_EXECUTABLE_SUFFIX}"
-    )
-    add_dependencies(${_executable_target} ${ARG_INVOCATION_TARGET})
-  endforeach()
-endfunction()

build_tools/github_actions/build_dist.py
@@ -47,12 +47,9 @@ a directory and:
 
   python ./main_checkout/build_tools/github_actions/build_dist.py main-dist
   python ./main_checkout/build_tools/github_actions/build_dist.py py-runtime-pkg
-  python ./main_checkout/build_tools/github_actions/build_dist.py
-  py-xla-compiler-tools-pkg
-  python ./main_checkout/build_tools/github_actions/build_dist.py
-  py-tflite-compiler-tools-pkg
-  python ./main_checkout/build_tools/github_actions/build_dist.py
-  py-tf-compiler-tools-pkg
+  python ./main_checkout/build_tools/github_actions/build_dist.py py-xla-compiler-tools-pkg
+  python ./main_checkout/build_tools/github_actions/build_dist.py py-tflite-compiler-tools-pkg
+  python ./main_checkout/build_tools/github_actions/build_dist.py py-tf-compiler-tools-pkg
 
 
 That is not a perfect approximation but is close.
@@ -72,6 +69,7 @@ WORK_DIR = os.path.realpath(os.path.curdir)
 BUILD_DIR = os.path.join(WORK_DIR, "iree-build")
 INSTALL_DIR = os.path.join(WORK_DIR, "iree-install")
 IREESRC_DIR = os.path.join(WORK_DIR, "main_checkout")
+TF_INTEGRATIONS_DIR = os.path.join(IREESRC_DIR, "integrations/tensorflow")
 BINDIST_DIR = os.environ.get("BINDIST_DIR")
 if BINDIST_DIR is None:
   BINDIST_DIR = os.path.join(WORK_DIR, "bindist")
@@ -119,7 +117,7 @@ def build_main_dist():
 
   # CMake configure.
   print("*** Configuring ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       f"-B{BUILD_DIR}",
@@ -128,17 +126,19 @@ def build_main_dist():
       f"-DIREE_BUILD_COMPILER=ON",
       f"-DIREE_BUILD_PYTHON_BINDINGS=ON",
       f"-DIREE_BUILD_SAMPLES=OFF",
-  ])
+  ],
+                 check=True)
 
   print("*** Building ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       "--build",
       BUILD_DIR,
       "--target",
       INSTALL_TARGET,
-  ])
+  ],
+                 check=True)
 
   print("*** Packaging ***")
   dist_entries = [
@@ -169,7 +169,7 @@ def build_py_runtime_pkg():
 
   # CMake configure.
   print("*** Configuring ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       f"-B{BUILD_DIR}",
@@ -179,17 +179,48 @@ def build_py_runtime_pkg():
       f"-DIREE_BUILD_PYTHON_BINDINGS=ON",
       f"-DIREE_BUILD_SAMPLES=OFF",
       f"-DIREE_BUILD_TESTS=OFF",
-  ])
+  ],
+                 check=True)
 
   print("*** Building ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       "--build",
       BUILD_DIR,
       "--target",
       "install-IreePythonPackage-rt-stripped",
-  ])
+  ],
+                 check=True)
+
+
+def bazel_build_tf_binary(target):
+  """Builds a binary in the IREE-TF Workspace and returns the filepath."""
+
+  # Builds a runnable target and returns the path to the executable. Yes this is
+  # really the best Bazel gives us.
+  # See https://github.com/bazelbuild/bazel/issues/8739
+  cmd = [
+      "bazel",
+      "run",
+      "--run_under=echo",
+      "--config=release",
+      target,
+  ]
+  process = subprocess.run(cmd,
+                           cwd=TF_INTEGRATIONS_DIR,
+                           check=True,
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True)
+
+  if len(process.stdout.splitlines()) != 1:
+    raise RuntimeError(
+        f"Unexpected output from `{' '.join(cmd)}`:\n{process.stdout}")
+  bin_path = process.stdout.strip()
+  if not os.path.isfile(bin_path):
+    raise RuntimeError("{bin_path} is not a file.")
+
+  return bin_path
 
 
 def build_py_xla_compiler_tools_pkg():
@@ -198,29 +229,35 @@ def build_py_xla_compiler_tools_pkg():
   shutil.rmtree(INSTALL_DIR, ignore_errors=True)
   remove_cmake_cache()
 
+  print("*** Building XLA import tool with Bazel ***")
+  binpath = bazel_build_tf_binary("//iree_tf_compiler:iree-import-xla")
+
   # CMake configure.
   print("*** Configuring ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       f"-B{BUILD_DIR}",
+      f"-DIREE_TF_TOOLS_ROOT={os.path.dirname(binpath)}",
       f"-DCMAKE_INSTALL_PREFIX={INSTALL_DIR}",
       f"-DCMAKE_BUILD_TYPE=Release",
       f"-DIREE_BUILD_XLA_COMPILER=ON",
       f"-DIREE_BUILD_PYTHON_BINDINGS=ON",
       f"-DIREE_BUILD_SAMPLES=OFF",
       f"-DIREE_BUILD_TESTS=OFF",
-  ])
+  ],
+                 check=True)
 
   print("*** Building ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       "--build",
       BUILD_DIR,
       "--target",
       "install-IreePythonPackage-tools-xla-stripped",
-  ])
+  ],
+                 check=True)
 
 
 def build_py_tflite_compiler_tools_pkg():
@@ -229,29 +266,35 @@ def build_py_tflite_compiler_tools_pkg():
   shutil.rmtree(INSTALL_DIR, ignore_errors=True)
   remove_cmake_cache()
 
+  print("*** Building TFLite import tool with Bazel ***")
+  binpath = bazel_build_tf_binary("//iree_tf_compiler:iree-import-tflite")
+
   # CMake configure.
   print("*** Configuring ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       f"-B{BUILD_DIR}",
+      f"-DIREE_TF_TOOLS_ROOT={os.path.dirname(binpath)}",
       f"-DCMAKE_INSTALL_PREFIX={INSTALL_DIR}",
       f"-DCMAKE_BUILD_TYPE=Release",
       f"-DIREE_BUILD_TFLITE_COMPILER=ON",
       f"-DIREE_BUILD_PYTHON_BINDINGS=ON",
       f"-DIREE_BUILD_SAMPLES=OFF",
       f"-DIREE_BUILD_TESTS=OFF",
-  ])
+  ],
+                 check=True)
 
   print("*** Building ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       "--build",
       BUILD_DIR,
       "--target",
       "install-IreePythonPackage-tools-tflite-stripped",
-  ])
+  ],
+                 check=True)
 
 
 def build_py_tf_compiler_tools_pkg():
@@ -260,12 +303,16 @@ def build_py_tf_compiler_tools_pkg():
   shutil.rmtree(INSTALL_DIR, ignore_errors=True)
   remove_cmake_cache()
 
+  print("*** Building TF import tool with Bazel ***")
+  binpath = bazel_build_tf_binary("//iree_tf_compiler:iree-tf-import")
+
   # CMake configure.
   print("*** Configuring ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       f"-B{BUILD_DIR}",
+      f"-DIREE_TF_TOOLS_ROOT={os.path.dirname(binpath)}",
       f"-DCMAKE_INSTALL_PREFIX={INSTALL_DIR}",
       f"-DCMAKE_BUILD_TYPE=Release",
       f"-DIREE_BUILD_TENSORFLOW_COMPILER=ON",
@@ -275,14 +322,15 @@ def build_py_tf_compiler_tools_pkg():
   ])
 
   print("*** Building ***")
-  subprocess.check_call([
+  subprocess.run([
       sys.executable,
       CMAKE_CI_SCRIPT,
       "--build",
       BUILD_DIR,
       "--target",
       "install-IreePythonPackage-tools-tf-stripped",
-  ])
+  ],
+                 check=True)
 
 
 command = sys.argv[1]

build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-swiftshader/build.sh
@@ -36,37 +36,35 @@ python3 -c 'import jax; print(jax.__version__)'
 # Print SwiftShader git commit
 cat /swiftshader/git-commit
 
-# TODO(4195): TensorFlow integrations can only be built out-of-tree (and kokoro
-# does not support that).
-# Temporarily get around these conflicting restrictions by nesting the git
-# repo under `/src/github/iree/iree`
-echo "Nesting iree.git into /src/github/iree/iree"
-mv iree iree-core  # Temporarily move iree/ so that we move the repo under it.
-shopt -s dotglob nullglob  # Make globs include dotfiles/dirs like `.git`.
-mkdir iree
-mv * ./iree | true  # | true to catch "can't mv ./iree into itself"
-cd iree
-mv iree-core iree # Move iree-core/ back to iree/
-ls -1 -a
-
 echo "Initializing submodules"
 ./scripts/git/submodule_versions.py init
+
+# BUILD the integrations binaries with Bazel
+pushd integrations/tensorflow
+BAZEL_CMD=(bazel --noworkspace_rc --bazelrc=build_tools/bazel/iree-tf.bazelrc)
+BAZEL_BINDIR="$(${BAZEL_CMD[@]?} info bazel-bin)"
+"${BAZEL_CMD[@]?}" build --config=generic_clang //iree_tf_compiler:all
+popd
+
+CMAKE_BUILD_DIR="$HOME/iree/build/tf"
 
 # TODO(gcmn): It would be nice to be able to build and test as much as possible,
 # so a build failure only prevents building/testing things that depend on it and
 # we can still run the other tests.
 echo "Configuring CMake"
-"${CMAKE_BIN}" -B ../iree-build/ -G Ninja \
+"${CMAKE_BIN}" -B "${CMAKE_BUILD_DIR?}" -G Ninja \
+   -DIREE_TF_TOOLS_ROOT="${BAZEL_BINDIR?}/iree_tf_compiler/" \
    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
    -DIREE_BUILD_COMPILER=ON \
    -DIREE_BUILD_TESTS=ON \
    -DIREE_BUILD_SAMPLES=OFF \
    -DIREE_BUILD_DEBUGGER=OFF \
    -DIREE_BUILD_XLA_COMPILER=ON \
+   -DIREE_BUILD_TFLITE_COMPILER=ON \
    -DIREE_BUILD_TENSORFLOW_COMPILER=ON .
 
 echo "Building with Ninja"
-cd ../iree-build/
+cd "${CMAKE_BUILD_DIR?}"
 ninja
 
 echo "Testing with CTest"

build_tools/kokoro/gcp_ubuntu/cmake-bazel/linux/x86-turing/build.sh
@@ -37,37 +37,37 @@ nvidia-smi || true
 
 ./build_tools/kokoro/gcp_ubuntu/check_vulkan.sh
 
-# TODO(4195): TensorFlow integrations can only be built out-of-tree (and kokoro
-# does not support that).
-# Temporarily get around these conflicting restrictions by nesting the git
-# repo under `/src/github/iree/iree`
-echo "Nesting iree.git into /src/github/iree/iree"
-mv iree iree-core  # Temporarily move iree/ so that we move the repo under it.
-shopt -s dotglob nullglob  # Make globs include dotfiles/dirs like `.git`.
-mkdir iree
-mv * ./iree | true  # | true to catch "can't mv ./iree into itself"
-cd iree
-mv iree-core iree # Move iree-core/ back to iree/
-ls -1 -a
-
 echo "Initializing submodules"
 ./scripts/git/submodule_versions.py init
+
+# BUILD the integrations binaries with Bazel
+pushd integrations/tensorflow
+BAZEL_CMD=(bazel --noworkspace_rc --bazelrc=build_tools/bazel/iree-tf.bazelrc)
+BAZEL_BINDIR="$(${BAZEL_CMD[@]?} info bazel-bin)"
+"${BAZEL_CMD[@]?}" build --config=generic_clang //iree_tf_compiler:all
+popd
+
+
+
+CMAKE_BUILD_DIR="$HOME/iree/build/tf"
 
 # TODO(gcmn): It would be nice to be able to build and test as much as possible,
 # so a build failure only prevents building/testing things that depend on it and
 # we can still run the other tests.
 echo "Configuring CMake"
-"${CMAKE_BIN}" -B ../iree-build/ -G Ninja \
+"${CMAKE_BIN}" -B "${CMAKE_BUILD_DIR?}" -G Ninja \
+   -DIREE_TF_TOOLS_ROOT="${BAZEL_BINDIR?}/iree_tf_compiler/" \
    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
    -DIREE_BUILD_COMPILER=ON \
    -DIREE_BUILD_TESTS=ON \
    -DIREE_BUILD_SAMPLES=OFF \
    -DIREE_BUILD_DEBUGGER=OFF \
    -DIREE_BUILD_XLA_COMPILER=ON \
+   -DIREE_BUILD_TFLITE_COMPILER=ON \
    -DIREE_BUILD_TENSORFLOW_COMPILER=ON .
 
 echo "Building with Ninja"
-cd ../iree-build/
+cd "${CMAKE_BUILD_DIR?}"
 ninja
 
 echo "Testing with CTest"

commit 167d8b9ae50d2dcbd99837e260d353b0e2f2a119

No deprecation warnings and no release asserts (#4965)
Fix release builds to build without asserts and turn off annoying
deprecation warnings.

The former is fallout from https://github.com/google/iree/pull/4836 and
the latter is something we've noticed being really annoying recently.

The setting of deprecation to no-error (explicitly inviting warning
spam) is something we seem to have inherited from Google's internal
build. My best guess is that people mostly only see these in the code
they're changing because everything is cached or something. If we want
to surface these warnings, we should set up clang-tidy. Spamming the
logs for anyone building anything in the project is not the way.

build_tools/bazel/iree.bazelrc
@@ -97,7 +97,7 @@ build:generic_clang --copt=-Wall
 # ratio.
 build:generic_clang --copt=-Wno-ambiguous-member-template
 build:generic_clang --copt=-Wno-char-subscripts
-build:generic_clang --copt=-Wno-error=deprecated-declarations
+build:generic_clang --copt=-Wno-deprecated-declarations
 build:generic_clang --copt=-Wno-extern-c-compat # Matches upstream. Cannot impact due to extern C inclusion method.
 build:generic_clang --copt=-Wno-gnu-alignof-expression
 build:generic_clang --copt=-Wno-gnu-variable-sized-type-not-at-end
@@ -161,8 +161,10 @@ build:macos_clang --per_file_copt=tensorflow,iree_tf_compiler@-Wno-range-loop-an
 # causes flags to be passed twice.
 ###############################################################################
 build:generic_clang_release --compilation_mode=opt
+build:generic_clang_release --copt=-DNDEBUG
 build:generic_clang_release --linkopt=-Wl,--strip-all
 build:generic_gcc_release --compilation_mode=opt
+build:generic_gcc_release --copt=-DNDEBUG
 build:generic_gcc_release --linkopt=-Wl,--strip-all
 # MSVC doesn't store symbols in the binary (they're in separate .pdb files), so
 # nothing needs to be stripped.

build_tools/cmake/iree_copts.cmake
@@ -106,7 +106,7 @@ iree_select_compiler_opts(IREE_DEFAULT_COPTS
     # signal/noise ratio.
     "-Wno-ambiguous-member-template"
     "-Wno-char-subscripts"
-    "-Wno-error=deprecated-declarations"
+    "-Wno-deprecated-declarations"
     "-Wno-extern-c-compat" # Matches upstream. Cannot impact due to extern C inclusion method.
     "-Wno-gnu-alignof-expression"
     "-Wno-gnu-variable-sized-type-not-at-end"